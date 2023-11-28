de3ebbc5538e3a9fbb40462c3122b0fe7c467d23
Ali <ialimalik99@gmail.com>
2023-11-28 12:32:05 -0500

    migrate codebase to CSS

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "../AuthInput.css";
 import styled, { css } from "styled-components";
+import Button from "./Button";
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -18,24 +19,31 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const ControlContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    margin-bottom: 1.5rem;
-  `;
-
+  let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase";
+  let inputClasses =
+    "w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow";
   const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
+  if (emailNotValid) {
+    labelClasses += " text-red-400";
+    inputClasses += " text-red-500 bg-red-100 border-red-300";
+  } else {
+    labelClasses += " text-stone-300";
+    inputClasses += " text-gray-700 bg-stone-300";
+  }
+
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800 mx-auto"
+    >
+      <div className="flex flex-col gap-2 mb-6">
         <p>
-          <label>Email</label>
+          <label className={labelClasses}>Email</label>
           <input
             type="email"
-            className={emailNotValid ? "invalid" : undefined}
+            className={inputClasses}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
@@ -43,20 +51,18 @@ export default function AuthInputs() {
           <label>Password</label>
           <input
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            className={inputClasses}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
           />
         </p>
-        </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
-        </button>
-        <button className="text-button" onClick={handleLogin}>
+      </div>
+      <div className="flex justify-end gap-4">
+        <Button type="button" className="text-amber-400 hover:text-amber-500">Create a new Account</Button>
+        <Button type="button" className="text-amber-400 hover:text-amber-500" onClick={handleLogin}>
           Sign In
-        </button>
+        </Button>
       </div>
     </div>
   );
